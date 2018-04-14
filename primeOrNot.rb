6cd4acc33d041f0a7a77581f28dfe0291994c40a
zapg <812929004@qq.com>
2018-04-14 10:28:00 +0800

    Add files via upload

---
+++ primeOrNot.rb
@@ -0,0 +1,19 @@
+
+num = ARGV[0].to_i 
+two = 2
+
+if num <= 0
+  puts "请输入自然数。"
+  return
+end
+
+  
+while two < num
+  if num % two != 0
+    two = two + 1
+  else 
+    puts "#{num}不是质数。"
+	return
+  end
+end
+puts "#{num}是质数。"
